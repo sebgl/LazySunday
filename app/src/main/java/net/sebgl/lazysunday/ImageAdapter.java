af4f5504b5142d1e351e54795f4aa3043da26fae
sebgl <zaolun@gmail.com>
2015-10-24 19:58:25 +0200

    set up gridView with mock data (dogs !)

---
+++ app/src/main/java/net/sebgl/lazysunday/ImageAdapter.java
@@ -0,0 +1,62 @@
+package net.sebgl.lazysunday;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by zao on 24/10/15.
+ */
+public class ImageAdapter extends BaseAdapter {
+
+    private Context context;
+    private final String LOG_TAG = this.getClass().getSimpleName();
+
+    private static final int[] mockThumbnailsId = {
+        R.drawable.sample_0, R.drawable.sample_1,
+        R.drawable.sample_2, R.drawable.sample_3,
+        R.drawable.sample_4, R.drawable.sample_5,
+        R.drawable.sample_6, R.drawable.sample_7
+    };
+
+    public ImageAdapter(Context context) {
+        super();
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return mockThumbnailsId.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View movieItem;
+        if (convertView == null){
+            LayoutInflater inflater = LayoutInflater.from(context);
+            movieItem = inflater.inflate(R.layout.movie_grid_item, parent, false);
+        }
+        else {
+            movieItem = convertView;
+        }
+        ImageView movieImageView = (ImageView)movieItem.findViewById(R.id.movieImageView);
+        TextView movieTextView = (TextView)movieItem.findViewById(R.id.movieTextView);
+        movieImageView.setImageResource(mockThumbnailsId[position]);
+        movieTextView.setText(""+position);
+        return movieItem;
+    }
+}
---
+++ app/src/main/java/net/sebgl/lazysunday/MovieGrid.java
@@ -0,0 +1,119 @@
+package net.sebgl.lazysunday;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link MovieGrid.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link MovieGrid#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MovieGrid extends Fragment {
+
+
+    public MovieGrid() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        GridView gridView = (GridView)inflater.inflate(R.layout.fragment_movie_grid, container, false);
+
+//        String[] mockStringData = {"toto", "titi", "tata", "tutu", "toutou"};
+//        // set gridView adapter
+//        ArrayAdapter<String> movieAdapter = new ArrayAdapter<>(getActivity(), R.layout.textview_listitem,
+//                mockStringData);
+        gridView.setAdapter(new ImageAdapter(getActivity()));
+
+        return gridView;
+    }
+
+
+
+
+
+
+
+
+
+
+//    // TODO: Rename parameter arguments, choose names that match
+//    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+//    private static final String ARG_PARAM1 = "param1";
+//    private static final String ARG_PARAM2 = "param2";
+//
+//    // TODO: Rename and change types of parameters
+//    private String mParam1;
+//    private String mParam2;
+//
+//    private OnFragmentInteractionListener mListener;
+//
+//    /**
+//     * Use this factory method to create a new instance of
+//     * this fragment using the provided parameters.
+//     *
+//     * @param param1 Parameter 1.
+//     * @param param2 Parameter 2.
+//     * @return A new instance of fragment MovieGrid.
+//     */
+//    // TODO: Rename and change types and number of parameters
+//    public static MovieGrid newInstance(String param1, String param2) {
+//        MovieGrid fragment = new MovieGrid();
+//        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
+
+//    @Override
+//    public void onAttach(Activity activity) {
+//        super.onAttach(activity);
+//        try {
+//            mListener = (OnFragmentInteractionListener) activity;
+//        } catch (ClassCastException e) {
+//            throw new ClassCastException(activity.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+//    }
+//
+//    @Override
+//    public void onDetach() {
+//        super.onDetach();
+//        mListener = null;
+//    }
+
+//    /**
+//     * This interface must be implemented by activities that contain this
+//     * fragment to allow an interaction in this fragment to be communicated
+//     * to the activity and potentially other fragments contained in that
+//     * activity.
+//     * <p/>
+//     * See the Android Training lesson <a href=
+//     * "http://developer.android.com/training/basics/fragments/communicating.html"
+//     * >Communicating with Other Fragments</a> for more information.
+//     */
+//    public interface OnFragmentInteractionListener {
+//        // TODO: Update argument type and name
+//        public void onFragmentInteraction(Uri uri);
+//    }
+
+}
